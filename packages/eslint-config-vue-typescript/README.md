4a79b73f334e720e02eac20e992cd3d85503a271
rhangai <hangai.renan@gmail.com>
2021-06-10 09:25:34 -0300

    chore: Added eslint-config-vue-typescript

--- packages/eslint-config-vue-typescript/README.md
+++ packages/eslint-config-vue-typescript/README.md
@@ -1,23 +1,18 @@
 ```sh
 yarn add --dev eslint \
   prettier \
+  eslint-config-airbnb-base \
+  eslint-plugin-import \
   eslint-plugin-prettier \
   eslint-plugin-vue \
   @typescript-eslint/eslint-plugin \
   @typescript-eslint/parser \
-  @vue/eslint-config-airbnb \
   @vue/eslint-config-prettier \
   @vue/eslint-config-typescript
 ```
 
 ```js
 module.exports = {
-	extends: [
-		'plugin:vue/essential',
-		'@vue/airbnb',
-		'@vue/typescript/recommended',
-		'@vue/prettier',
-		'@vue/prettier/@typescript-eslint',
-	],
+	extends: ['@rhangai/vue-typescript'],
 };
 ```
---
+++ packages/eslint-config-vue-typescript/index.js
@@ -0,0 +1,83 @@
+module.exports = {
+	extends: [
+		'plugin:vue/essential',
+		'eslint-config-airbnb-base',
+		'@vue/typescript/recommended',
+		'@vue/prettier',
+		'@vue/prettier/@typescript-eslint',
+	],
+	settings: {
+		'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			},
+		},
+	},
+	rules: {
+		'no-void': ['error', { allowAsStatement: true }],
+		'no-use-before-define': ['error', 'nofunc'],
+		'no-shadow': 'off',
+		'@typescript-eslint/no-floating-promises': 'error',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-shadow': ['error'],
+		'import/prefer-default-export': 'off',
+		'import/extensions': [
+			'error',
+			{
+				'.js': 'never',
+				'.jsx': 'never',
+				'.ts': 'never',
+				'.tsx': 'never',
+				'.json': 'always',
+				'.vue': 'always',
+			},
+		],
+		'import/no-unresolved': [
+			'error',
+			{
+				ignore: ['^@@', '^~'],
+			},
+		],
+		'import/order': [
+			'error',
+			{
+				groups: [
+					//
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index',
+				],
+				pathGroups: [
+					{
+						pattern: '{vue-demi,@vue/composition-api}',
+						group: 'external',
+						position: 'before',
+					},
+					{
+						pattern: '@nuxtjs/composition-api',
+						group: 'external',
+						position: 'before',
+					},
+					{
+						pattern: '{@@**,@@*/**}',
+						group: 'internal',
+						position: 'before',
+					},
+					{
+						pattern: '{~/**,~/*/**}',
+						group: 'internal',
+					},
+				],
+				pathGroupsExcludedImportTypes: ['builtin'],
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+			},
+		],
+	},
+};
