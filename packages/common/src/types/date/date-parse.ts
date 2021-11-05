03d9c5a86a9d1d146e9af6610446aef74e45a85e
rhangai <hangai.renan@gmail.com>
2021-11-05 14:31:06 -0300

    chore: Added dateTryParse

--- packages/common/src/types/date/date-parse.ts
+++ packages/common/src/types/date/date-parse.ts
@@ -5,16 +5,22 @@ export type DateParseOptions = {
 	inputFormat?: string;
 };
 
-export function dateParse(
+export function dateTryParse(
 	param: DateTypeInput,
 	{ inputFormat = 'YYYY-MM-DD' }: DateParseOptions
-): DateType {
+): DateType | null {
 	let date: DateType;
 	if (typeof param === 'string') {
 		date = dayjs(param, inputFormat);
 	} else {
 		date = dayjs(param);
 	}
-	if (!date.isValid()) throw new Error(`Invalid date: ${param}`);
+	if (!date.isValid()) return null;
+	return date;
+}
+
+export function dateParse(param: DateTypeInput, options: DateParseOptions): DateType {
+	const date = dateTryParse(param, options);
+	if (!date) throw new Error(`Invalid date: ${param}`);
 	return date;
 }
